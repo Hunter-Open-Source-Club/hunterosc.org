ae00dc655c8dd4a1d02db782efe02e35e301aa71
Blake Vente <dev@rvente.com>
2020-07-27 00:44:59 -0400

    add gatsby-plugin-them-ui for shadowing

--- src/@narative/gatsby-theme-novela/components/Logo/index.js
+++ src/@narative/gatsby-theme-novela/components/Logo/index.js
@@ -24,7 +24,7 @@ const Logo = ({ fill = "white" }) => {
     <LogoContainer>
       <svg
         width="400"
-        height='auto'
+        height='48'
         viewBox="0 0 192 23"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
